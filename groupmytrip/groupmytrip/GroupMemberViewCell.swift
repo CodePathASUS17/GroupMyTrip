91482f83d30b5bd5d590ff7264faa7ecd0a5ec95
panopticons <thepanopticons@gmail.com>
2017-03-11 16:56:52 -0700

    trips detail view updated

---
+++ groupmytrip/groupmytrip/GroupMemberViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroupMemberViewCell.swift
+//  groupmytrip
+//
+//  Created by fer on 3/11/17.
+//  Copyright © 2017 Diana C. All rights reserved.
+//
+
+import UIKit
+
+class GroupMemberViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
